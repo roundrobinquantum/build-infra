b07312d460e62bcaa09a1d703981ec970b8db284
sguney <serhan.guney@hepsiburada.com>
2018-03-11 19:13:47 +0300

    #000 serhan refactors disposing system design. Changed all disposing commands to wait until commands

--- dispose.sh
+++ dispose.sh
@@ -7,31 +7,24 @@ function dispose() {
   # Remove all stacks in attached build network
   remove_stacks
 
-  # Wait for docker's gracefully stack removing 
-  sleep 10
+  # Wait until all stacks removed
+  wait_until_stacks_are_removed
 
   # prune the system with force
-  docker_prune
+  docker_system_prune
   
   # clean up volumes
-  docker volume prune --force
+  docker_volume_prune
 
-  # last cleanup
-  docker_prune
+  # remove non using images in build network
+  remove_non_using_images
 
-  sleep 15
-
-  # remove images
-  docker rmi -f $(docker image ls -aq)
-
-  docker volume prune --force
-
-  echo "cleanup complete. Deleted all volumes and all services"
+  echo "Disposing complete. Deleted all volumes, all images and all services attached to build network."
 }
 
 function remove_stacks() {
 
-  STACKS_IN_BUILD_NETWORK=$(docker ps --filter network=build --format '{{.Label "com.docker.stack.namespace"}}')
+  export STACKS_IN_BUILD_NETWORK=$(docker ps --filter network=build --format '{{.Label "com.docker.stack.namespace"}}')
 
   if [[ $STACKS_IN_BUILD_NETWORK ]]; then
      echo "Removing all stacks in attached build network"
@@ -42,8 +35,42 @@ function remove_stacks() {
   fi
 }
 
-function docker_prune() {
+function wait_until_stacks_are_removed() {
+
+  STACKS_ARE_REMAINING=false
+
+  echo "waiting for gracefully shutting down stacks"
+
+  until [ "${STACKS_ARE_REMAINING}" == "true" ] 
+  do
+    INITIAL_STATE_OF_STACKS=$(docker ps --filter network=build --format '{{.Label "com.docker.stack.namespace"}}')
+
+    if [[ $INITIAL_STATE_OF_STACKS ]]; then
+      STACKS_ARE_REMAINING=false
+    else
+      STACKS_ARE_REMAINING=true
+    fi
+  done
+}
+
+function docker_system_prune() {
   docker system prune --force
+}
+
+function docker_volume_prune() {
+  docker volume prune --force
+}
+
+function remove_non_using_images(){
+
+  IMAGES_IN_BUILD_NETWORK=$(docker ps --filter network=build --format '{{.Image}}')
+
+  if [[ ${IMAGES_IN_BUILD_NETWORK} ]]; then
+    echo "removing non using images"
+    docker rmi -f ${IMAGES_IN_BUILD_NETWORK}
+  else
+    echo "Non using images not found... Moving on..."
+  fi
 }
 
 read -p "You're about to dispose the entire core build infrastructure!!!.This task will remove all docker stacks and your all images. Do you want to continue? (yes/no)" choice
