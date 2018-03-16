b91703744a7d4592d39da20b659380adc80be89e
sguney <serhan.guney@hepsiburada.com>
2018-03-17 01:51:46 +0300

    #000 serhan updates gocd server version from 18.1 to 18.2

--- gocd-server/image/create.sh
+++ gocd-server/image/create.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker pull gocd/gocd-server:v18.1.0
-docker tag gocd/gocd-server:v18.1.0 mpl-dockerhub.hepsiburada.com/gocd-server:v18.1.0
-docker push mpl-dockerhub.hepsiburada.com/gocd-server:v18.1.0
+docker pull gocd/gocd-server:v18.2.0
+docker tag gocd/gocd-server:v18.2.0 mpl-dockerhub.hepsiburada.com/gocd-server:v18.2.0
+docker push mpl-dockerhub.hepsiburada.com/gocd-server:v18.2.0
